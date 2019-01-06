8c302795776d83a6e91d8958dd3eadcc10d8f830
Anthony Hillen <ah205@students.uwf.edu>
2019-01-06 14:20:46 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
-
+row = ["   |   |   ","-----------","   |   |   ","-----------","   |   |   "]
 def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  puts row[0]
+  puts row[1]
+  puts row[2]
+  puts row[3]
+  puts row[4]
 end
